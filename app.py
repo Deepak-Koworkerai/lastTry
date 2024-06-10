ac0e806be33b4e9f641482c997a38011b4b89a29
Deepak-Koworkerai <165667192+Deepak-Koworkerai@users.noreply.github.com>
2024-06-10 20:11:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def user_input(user_question):
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     logger.info("-------------------------DATABASE LOADED!!!--------------------------")    
     # Search for similar documents
-    docs = new_db.similarity_search(user_question,k=1)
+    docs = new_db.similarity_search(user_question,k=3)
     logger.info("-------------------------RETRIEVED SIMILAR DATA!!!--------------------------") 
     context = " ".join([doc.page_content for doc in docs])
     print(context)
